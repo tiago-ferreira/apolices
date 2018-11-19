d827b88b54d208c6988daff84d8721e83745f95e
tiago <si.tiagoferreira@gmail.com>
2018-11-18 23:04:18 -0200

    add query params

--- src/main/java/br/com/corretora/apolices/apolices/controller/ApoliceController.java
+++ src/main/java/br/com/corretora/apolices/apolices/controller/ApoliceController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.corretora.apolices.apolices.model.Apolice;
@@ -19,7 +20,10 @@ public class ApoliceController {
 	
 	
 	@GetMapping
-	public List<Apolice> getApolices() {
+	public List<Apolice> getApolices(@RequestParam("cpf") String cpf,@RequestParam(value="documento_vigente", required=false) String documento_vigente,@RequestParam(value="companhia", required=false) String companhia) {
+		System.out.println("CPF: "+cpf);
+		System.out.println("Documento Vigente: "+documento_vigente);
+		System.out.println("Companhia: "+companhia);
 		return apoliceService.findAll();
 	}
 	
--- src/main/java/br/com/corretora/apolices/apolices/repository/ApoliceRepository.java
+++ src/main/java/br/com/corretora/apolices/apolices/repository/ApoliceRepository.java
@@ -17,8 +17,8 @@ public class ApoliceRepository {
 	
 	static {
 		
-		apolices.add(new Apolice("individual", "01.68.4002035.0000000.1", "Joaquim Jose", "Itau Seguros", "ACID. PESSOAIS", "Vida", LocalDateTime.now(), LocalDateTime.now(), true, "Itau Corretora de Seguros", "97.619.507/0001-60", "31.840.059/0001-11", "3121.840.0592.2340001.2311", "N", new BigDecimal("19.00"), new BigDecimal("20.000")));
-		apolices.add(new Apolice("certificado", "01.68.4002036.0000000.1", "Joao Severo", "Itau Seguros", "Auto", "Auto", LocalDateTime.now(), LocalDateTime.now(), true, "Itau Corretora de Seguros", "97.619.507/0001-60", "31.840.059/0001-11", "3121.840.0592.2340001.2311", "N", new BigDecimal("30.00"), new BigDecimal("30.000")));
+		apolices.add(new Apolice("individual", "01.68.4002035.0000000.1", "Joaquim Jose", "Itau Seguros", "Seguro Vida", "Vida", LocalDateTime.now(), LocalDateTime.now(), true, "Itau Corretora de Seguros", "97.619.507/0001-60", "31.840.059/0001-11", "3121.840.0592.2340001.2311", "N", new BigDecimal("19.00"), new BigDecimal("20.000")));
+		apolices.add(new Apolice("certificado", "01.68.4002036.0000000.1", "Joao Severo", "Itau Seguros", "Seguro Auto", "Auto", LocalDateTime.now(), LocalDateTime.now(), true, "Itau Corretora de Seguros", "97.619.507/0001-60", "31.840.059/0001-11", "3121.840.0592.2340001.2311", "N", new BigDecimal("30.00"), new BigDecimal("30.000")));
 		apolices.add(new Apolice("individual", "01.68.4002037.0000000.1", "Abedias Candido", "Itau Seguros", "Seguro Viagem", "Viagem", LocalDateTime.now(), LocalDateTime.now(), true, "Itau Corretora de Seguros", "97.619.507/0001-60", "31.840.059/0001-11", "3121.840.0592.2340001.2311", "N", new BigDecimal("9.00"), new BigDecimal("9.000")));
 	
 	}
